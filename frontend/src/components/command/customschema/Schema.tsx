b2ae228e443b2602588eb95a2ad33ed6880c0cc9
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2024-10-09 12:52:37 -0500

    adding new block to schema

--- frontend/src/components/command/customschema/Schema.tsx
+++ frontend/src/components/command/customschema/Schema.tsx
@@ -5,11 +5,13 @@ import {
   defaultStyleSpecs,
 } from "@blocknote/core";
 import { Bubble } from "../customblocks/BubbleBlock";
+import { NoParam } from "../customblocks/NoParamBlock";
 
 export const schema = BlockNoteSchema.create({
   blockSpecs: {
     ...defaultBlockSpecs,
     bubble: Bubble,
+    noparam: NoParam,
   },
   inlineContentSpecs: {
     ...defaultInlineContentSpecs,
