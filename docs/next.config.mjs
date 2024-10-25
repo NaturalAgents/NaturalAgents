fb6d46c24d54b5e06f2c1ab95afd7e9d3a8f4066
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2024-10-25 18:23:17 -0400

    nextjs config for github hosting

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,4 +5,12 @@ const withNextra = nextra({
   themeConfig: "./theme.config.jsx",
 });
 
-export default withNextra();
+module.exports = {
+  ...withNextra(),
+  // 1. Set `basePath` since your docs are under a subpath
+  basePath: "/",
+  // 2. Enable static export
+  output: "export",
+  // 3. When Image Optimization using Next.js default loader is not compatible
+  images: { unoptimized: true },
+};
